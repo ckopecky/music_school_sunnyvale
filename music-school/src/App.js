86c4fb3229a5dd6db73fc61b37bf7eff7ad1b0e8
ckopecky <ckopecky@github.com>
2019-02-03 22:30:18 -0800

    dropdown menus work navbar styled
    TODO: Link primary-navbar to pages
    and search function

--- music-school/src/App.js
+++ music-school/src/App.js
@@ -3,14 +3,16 @@ import { Switch, Route } from 'react-router-dom';
 import logo from "./media/Music_School_Logo.png";
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
-import History from './components/History/History';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state ={
       primaryNav: ['Sign In', 'Stay Informed', 'Calendar', 'Location', 'Donate'],
-      secondaryNav: ['EDUCATION', 'PERFORMANCE', 'COMMUNITY']
+      secondaryNav: ['EDUCATION', 'PERFORMANCE', 'COMMUNITY'],
+      education: ['New to The Music School?', 'Enroll', 'Group Classes', 'Private Lessons', 'Summer Classes', 'Faculty'],
+      community: ['Partners', 'Resources', 'Need a Musician?'],
+      performance: ['Events Calendar', 'Student Recitals', 'Sunnyvale Presbyterian Church']
     }
   }
 
@@ -22,6 +24,9 @@ class App extends Component {
           <Navbar 
             nav={this.state.primaryNav}
             secondaryNav={this.state.secondaryNav}
+            education={this.state.education}
+            community={this.state.community}
+            performance={this.state.performance}
             logo={logo}
             />
 
--- music-school/src/components/Navbar/Navbar.js
+++ music-school/src/components/Navbar/Navbar.js
@@ -3,22 +3,53 @@ import "./Navbar.css";
 const Navbar = (props) => {
     return (
         <React.Fragment>
-            <nav className="navigation">
-                {props.nav.map(item => {
-                    return (
-                        <a className="nav-link" href={`#${item}`}>{item}</a>
-                    )
-                })}
+            <nav className="navbar navbar-expand-lg navbar-light navigation">  
+                <a className="navbar-brand" href="/">The Music School</a>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"><i className="fas fa-bars"></i></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarText">
+                    <ul className="navbar-nav mr-auto navigation">
+                        {props.nav.map((item, i) => {
+                            return (
+                                <li key={Math.random(i)}>
+                                    <a className="nav-link" href={`#${item}`}>{item}</a>
+                                </li>
+                            )
+                        })}
+                    </ul>
+                </div>
             </nav>
             <div className="secondary-navigation">
                 <img src={props.logo} alt="logo"/>
-                <div className="secondary-nav-link">
-                {props.secondaryNav.map(item => {
+                
+                
+                <ul className="secondary-navigation-ul">{props.secondaryNav.map((item, i) => {
+                    console.log((item, props));
                     return (
-                        <div className="secondary-nav-link-a">{item}</div>
+                        <li key={Math.random(i)} className="btn-group dropdown secondary-navigation">
+                            <button type="button" className="secondary-nav-button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <div className="secondary-nav-link-a">{item}</div>
+                            </button>
+                            <div className="dropdown-menu">
+                                <div className="dropdown-menu-item"> {item === 'EDUCATION' ? props.education.map((edlink, j )=> {
+                                    return (
+                                        <h6>{edlink}</h6>)
+                                }) : item === 'PERFORMANCE' ? props.performance.map((perflink, j )=> {
+                                    return (
+                                        <h6>{perflink}</h6>
+                                    )
+                                }): props.community.map((commlink, j )=> {
+                                    return (
+                                        <h6>{commlink}</h6>
+                                    )
+                                })}
+                                </div>
+                            </div>
+                        </li>
                     )
-                })}
-                </div>
+                    })}
+                    </ul>
                 <div className="secondary-nav-search">
                     <label>Search:</label>
                     <input type="search" placeholder="search"/>
